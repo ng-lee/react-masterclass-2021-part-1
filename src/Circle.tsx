01cac7b02c48776c1e1b59928f3dc7e6ba56355d
codeer <loginonlyyy@gmail.com>
2022-01-20 22:24:42 +0900

    usage of Optional Props

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -2,20 +2,24 @@ import styled from "styled-components";
 
 interface ContainerProps {
   bgColor: string;
+  borderColor: string;
 }
 
 const Container = styled.div<ContainerProps>`
   width: 200px;
   height: 200px;
   background-color: ${(props) => props.bgColor};
+  border-radius: 50%;
+  border: 3px solid ${(props) => props.borderColor};
 `;
 
 interface CircleProps {
   bgColor: string;
+  borderColor?: string;
 }
 
-function Circle({ bgColor }: CircleProps) {
-  return <Container bgColor={bgColor} />;
+function Circle({ bgColor, borderColor }: CircleProps) {
+  return <Container bgColor={bgColor} borderColor={borderColor ?? bgColor} />;
 }
 
 export default Circle;
