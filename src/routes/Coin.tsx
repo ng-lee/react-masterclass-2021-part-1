5678ba4cdb8051aea87d7d268f295a3f34613af9
codeer <loginonlyyy@gmail.com>
2022-01-23 20:58:09 +0900

    sent RouteState

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,50 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const Loading = styled.div`
+  align-items: center;
+`;
 
 interface RouteParams {
   coinId: string;
 }
 
+interface RouteState {
+  name: string;
+}
+
 function Coin() {
+  const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>();
-  return <h1>Coin: {coinId}</h1>;
+  const { state } = useLocation<RouteState>();
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading..."}</Title>
+      </Header>
+      {loading ? <Loading>Loading...</Loading> : null}
+    </Container>
+  );
 }
 
 export default Coin;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,8 +1,11 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
   padding: 0 20px;
+  max-width: 480px;
+  margin: 0 auto;
 `;
 
 const Header = styled.header`
@@ -20,6 +23,12 @@ const Title = styled.h1`
 
 const CoinsList = styled.ul``;
 
+const Img = styled.img`
+  width: 25px;
+  height: 25px;
+  margin-right: 8px;
+`;
+
 const Coin = styled.li`
   background-color: ${(props) => props.theme.textColor};
   color: ${(props) => props.theme.bgColor};
@@ -29,53 +38,63 @@ const Coin = styled.li`
   margin-bottom: 10px;
   font-size: 15px;
   font-weight: 600;
-  padding-left: 5px;
+  padding-left: 10px;
   border-radius: 5px;
 `;
 
-const coins = [
-  {
-    id: "btc-bitcoin",
-    name: "Bitcoin",
-    symbol: "BTC",
-    rank: 1,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "eth-ethereum",
-    name: "Ethereum",
-    symbol: "ETH",
-    rank: 2,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "usdt-tether",
-    name: "Tether",
-    symbol: "USDT",
-    rank: 3,
-    is_new: false,
-    is_active: true,
-    type: "token",
-  },
-];
+const Loading = styled.div`
+  align-items: center;
+`;
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
 
 function Coins() {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      setCoins(json.slice(0, 100));
+      setLoading((prev) => !prev);
+    })();
+  }, []);
   return (
     <Container>
       <Header>
         <Title>Coins List</Title>
       </Header>
-      <CoinsList>
-        {coins.map((coin) => (
-          <Link to={`/${coin.id}`}>
-            <Coin key={coin.id}>{coin.name} &rarr;</Coin>
-          </Link>
-        ))}
-      </CoinsList>
+      {loading ? (
+        <Loading>Loading...</Loading>
+      ) : (
+        <CoinsList>
+          {coins.map((coin) => (
+            <Link
+              to={{
+                pathname: `/${coin.id}`,
+                state: {
+                  name: coin.name,
+                },
+              }}
+            >
+              <Coin key={coin.id}>
+                <Img
+                  src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                />
+                {coin.name} &rarr;
+              </Coin>
+            </Link>
+          ))}
+        </CoinsList>
+      )}
     </Container>
   );
 }
