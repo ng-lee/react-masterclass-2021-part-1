4185c2784a543bdaca0e1a4335855d9fed9db594
codeer <loginonlyyy@gmail.com>
2022-01-23 21:37:37 +0900

    coin types

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -33,10 +33,65 @@ interface RouteState {
   name: string;
 }
 
+interface IInfo {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface IPriceInfo {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
+
 function Coin() {
   const [loading, setLoading] = useState(true);
-  const [info, setInfo] = useState({});
-  const [priceInfo, setPriceInfo] = useState({});
+  const [info, setInfo] = useState<IInfo>();
+  const [priceInfo, setPriceInfo] = useState<IPriceInfo>();
   const { coinId } = useParams<RouteParams>();
   const { state } = useLocation<RouteState>();
   useEffect(() => {
@@ -45,7 +100,7 @@ function Coin() {
         await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
       ).json();
       const priceData = await (
-        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinID}`)
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
       ).json();
       setInfo(infoData);
       setPriceInfo(priceData);
